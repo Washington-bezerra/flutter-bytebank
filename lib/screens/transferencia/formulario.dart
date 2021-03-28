19eb70b8c745d012b1d38d07562e86d23dd1f3e8
Washington-bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2021-03-27 23:22:25 -0300

    Inicial

---
+++ lib/screens/transferencia/formulario.dart
@@ -0,0 +1,53 @@
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencia.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Criando Tranferencia';
+
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+
+const _rotuloCampoNumeroConta = 'Número da conta';
+const _dicaCampoNumeroConta = '0000';
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumperoConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_tituloAppBar),
+        ),
+        body: Column(
+          children: <Widget>[
+            Editor(
+                controlador: _controladorCampoNumperoConta,
+                rotulo: _rotuloCampoNumeroConta,
+                dica: _dicaCampoNumeroConta),
+            Editor(
+                controlador: _controladorCampoValor,
+                rotulo: _rotuloCampoValor,
+                dica: _dicaCampoValor,
+                icone: Icons.monetization_on),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              child: Text(_textoBotaoConfirmar),
+              onPressed: () => _criaTranferencia(context),
+            ),
+          ],
+        ));
+  }
+
+  void _criaTranferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumperoConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
